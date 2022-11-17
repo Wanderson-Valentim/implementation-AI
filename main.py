a80eaff432ac8a29932ca15d2230805e3cac0a46
Wanderson Valentim <wanderson200108@gmail.com>
2022-11-16 22:54:05 -0300

    add generate_states function

--- main.py
+++ main.py
@@ -1,32 +1,58 @@
 import random
 
 class Board:
-  def __init__(self, dimension = 8) -> None:
+  def __init__(self, dimension = 8, positions = []) -> None:
     self.board = [[0 for x in range(dimension)] for i in range(dimension)]
+    self.board_states = [[0 for x in range(dimension)] for i in range(dimension)]
     self.dimension = dimension
+    self.__positions = positions
+    self.number_of_attacks = 0
     
-  def fill_board(self, positions):
-    for column in range(self.dimension):
-      row = positions[column]
-      self.board[row][column] = 1
-      
+  def get_positions(self):
+    return self.__positions
+  
+  def set_positions(self, positions):
+    self.__positions = positions
+    self.board = [[0 for x in range(self.dimension)] for i in range(self.dimension)]
+    self.fill_board()
+    self.__generate_state()
+    self.set_number_of_attacks()
+  
+  def generate_positions(self):
+    for i in range(self.dimension):
+      row = random.randint(0, (self.dimension - 1))
+      self.__positions.append(row)
+    
+    self.fill_board()
+    self.__generate_state()
+    self.set_number_of_attacks()
+    
   def print_board(self):
     for index in range(self.dimension):
       print(self.board[index])
   
-  def number_of_attacks(self, positions):
+  def print_board_states(self):
+    for index in range(self.dimension):
+      print(self.board_states[index])
+  
+  def fill_board(self):
+    for column in range(self.dimension):
+      row = self.__positions[column]
+      self.board[row][column] = 1
+  
+  def set_number_of_attacks(self):
     number_of_attacks = 0
     
     for column in range(self.dimension):
       if(column < (self.dimension - 1)):
-        row = positions[column]
+        row = self.__positions[column]
         
         number_of_attacks += self.__traverse_row(column, row)
         number_of_attacks += self.__traverse_diagonally_ascending(column, row)
         number_of_attacks += self.__traverse_diagonally_descending(column, row)
       
-    return number_of_attacks
-  
+    self.number_of_attacks = number_of_attacks
+
   def __traverse_diagonally_ascending(self, column, row):
     number_of_attacks = 0
     
@@ -61,27 +87,29 @@ class Board:
         number_of_attacks += 1
     
     return number_of_attacks
-      
 
-def generate_positions(board_dimension):
-  positions = []
-  #positions = [4,5,6,3,4,5,6,5]
-  
-  for i in range(board_dimension):
-    row = random.randint(0, (board_dimension - 1))
-    positions.append(row)
-    
-  return positions
-
+  def __generate_state(self):
+    for i in range(self.dimension):
+      for j in range(self.dimension):
+        copy_positions = self.__positions.copy()
+        copy_positions[i] = j
+        aux_board = Board(self.dimension, copy_positions)
+        aux_board.fill_board()
+        aux_board.set_number_of_attacks()
+        self.board_states[j][i] = aux_board.number_of_attacks
 
 def main():
   dimension = 8
-  board = Board(dimension)
-  positions = generate_positions(dimension)
-
-  print(positions)
-  board.fill_board(positions)
-  board.print_board()
-  print(board.number_of_attacks(positions))
+  game = Board(dimension)
+  game.generate_positions()
+  game.set_positions([4,5,6,3,4,5,6,5])
+  
+  minimum = 28
+  
+  for i in range(dimension):
+    for j in range(dimension):
+      if minimum > game.board_states[j][i]:
+        minimum = game.board_states[j][i]
+        index = (j,i)
 
 main()
